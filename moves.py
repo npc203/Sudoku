129f5f62aaf57c77ffab6c9663d0da86b4364c03
calebgood <50765275+calebgood@users.noreply.github.com>
2020-01-19 17:26:33 +0530

    Update moves.py

--- moves.py
+++ moves.py
@@ -14,27 +14,25 @@ pyautogui.moveTo(1169,617, duration = 0)
 
 
 def move(string):
-    cod=[int(i) for i in string.replace('(','').replace(')','').split(',')]
+    """Function takes tuple strings as input and moves to the approriate position"""
+    cod=(int(i) for i in string.replace('(','').replace(')','').split(',')])
     pyautogui.moveTo(cod[0],cod[1], duration = 0.01)
     
-arr=[]
+
+
 #Parsing
+arr=[]
 with open('coordinates.txt','r') as f:
     lines=f.readlines()
-    for i in lines:
-        move(i.replace('\n',''))
-
-'''
-    for i in range(9):
-        tmp=[]
-        for j in range(9):
-           tmp.append(lines[i+j].replace('\n',''))
-        arr.append(tmp)
-'''
+op=0
+for i in range(9):
+    tmp=[]
+    for j in range(9):
+       tmp.append(lines[op].replace('\n',''))
+       op+=1
+    arr.append(tmp)
 
 
-for i in arr:
-    for j in i:
-        move(j)
+
 
 
